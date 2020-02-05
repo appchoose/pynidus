322469de1ad638ad9e48352e62e51ec8cc798082
keurcien <keurcien.luu@gmail.com>
2020-02-05 12:10:51 +0100

    Added tests

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def dependencies():
+    import os
+    """
+    Obtain the dependencies from requirements.txt.
+    """
+    with open('requirements.txt') as reqs:
+        return reqs.read().splitlines()
+
 setuptools.setup(
     name="pynidus",
     version="0.0.119",
@@ -12,11 +20,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/appchoose/pynidus",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'pandas',
+        'elasticsearch',
+        'psycopg2',
+        'google-cloud-storage'
+    ]
 )
---
+++ tests/unit/test_clients.py
@@ -0,0 +1,11 @@
+import pytest
+from pynidus import clients
+
+def test_es_client():
+    pass
+
+def test_pg_client():
+    pass
+
+def test_gcs_client():
+    pass
