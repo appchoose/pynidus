bae40b41abd36ef2ad3bf1315cb66ce10347fbd2
keurcien <keurcien.luu@gmail.com>
2020-02-05 16:42:38 +0100

    Added model version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def dependencies():
 
 setuptools.setup(
     name="pynidus",
-    version="0.0.12",
+    version="0.0.120",
     author="Keurcien Luu",
     author_email="keurcien@appchoose.io",
     description="A handful of utilities.",
