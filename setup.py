01a6d033f3e1d9b08734892a23c4474d65b1a5f4
keurcien <keurcien.luu@gmail.com>
2020-02-07 15:02:28 +0100

    Added bugsnag

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def dependencies():
 
 setuptools.setup(
     name="pynidus",
-    version="0.0.123",
+    version="0.0.124",
     author="Keurcien Luu",
     author_email="keurcien@appchoose.io",
     description="A handful of utilities.",
